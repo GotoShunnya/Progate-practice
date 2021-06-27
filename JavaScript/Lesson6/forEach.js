ba8da9bd435623d56dd75983440db6655f7105a7
Gotoshunya <1800syunya@Gmail.com>
2021-06-27 15:04:06 +0900

    6/27 各メッソド練習

---
+++ JavaScript/Lesson6/forEach.js
@@ -0,0 +1,5 @@
+//forEachメソッド練習
+const characters = ["にんじゃわんこ", "ベイビーわんこ", "ひつじ仙人", "とりずきん"];
+
+
+characters.forEach((character) => {console.log(character)});
--- JavaScript/Lesson6/push.js
+++ JavaScript/Lesson6/push.js
@@ -6,9 +6,3 @@ console.log(characters);
 characters.push("とりずきん");
 
 console.log(characters);
-
-//forEachメソッド練習
-const characters = ["にんじゃわんこ", "ベイビーわんこ", "ひつじ仙人", "とりずきん"];
-
-
-characters.forEach((character) => {console.log(character)});
